5c58f290b1bfa3d8f291086f0c76f107ab552d55
Lorenzo Ruiz <lruizalfieri@gmail.com>
2022-04-26 17:43:46 +0200

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    $("#getusers").on("click", function(){
+        $.ajax({    
+            method: "GET",
+            url: "localhost:5000/users",
+        }).done(function (info) {
+            console.log("xd");
+            if (info["status"] == "OK"){
+                var array = [];
+                let users = [];
+                if (users != null) {
+                    array = users.split(',');
+                    for (i in array) {
+                        let newelem = $("<li><a href=''>"+array[i].username+'</a><button class="showweapons">Show Weapons</button></li>');
+                        $("ul").append(newelem);
+                        $(".ui-listview").listview("refresh");
+
+                        $(".showweapons").on('click', function() {
+                           
+                        });
+                    }
+                };
+            } else if (info["status"] != "OK"){
+                console.log(info)
+                alert(info["message"])
+            }
+        }).fail(function () {
+            console.log("URL incorrecta");
+            alert("URL incorrecta, por favor introduzca una url valida.");
+        });
+        return false;
+    });
+}
